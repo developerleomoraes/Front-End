4cc1b28ea62db8ff27dc5d09164bca2a2f915f48
leomoraes <developerleo.it@gmail.com>
2023-01-29 14:52:14 -0300

    JavaScript Updates 29.01

--- JavaScript/exe11/exe008.js
+++ JavaScript/exe11/exe008.js
@@ -1,1 +1,6 @@
-console.log('Hello World!')
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Diriga sempre usando cinto de segurança`)
---
+++ JavaScript/exe11/exe009.js
@@ -0,0 +1,7 @@
+var country = 'Brazil'
+console.log(`Vivendo em ${country}`)
+if(country == 'Brazil'){
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro')
+}
