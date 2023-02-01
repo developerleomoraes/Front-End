7aa05742f1204967f0fe48d8a4267016236559f0
leomoraes <developerleo.it@gmail.com>
2023-01-31 22:11:41 -0300

    JavaScript Updates - 31.01.23

--- JavaScript/aula15/ambience.js
+++ JavaScript/aula15/ambience.js
@@ -4,7 +4,15 @@ num.sort()
 console.log(num)
 console.log(`Nosso array tem ${num.length} posições`)
 console.log(`O primeiro valor do array é ${num[0]}`)
+let i = num.indexOf(7)
 
-for(let i=0; i<=num.length; i++){
+if(i == -1){
+    console.log('O valor não foi encontrado')
+} else{
+    console.log(`O valor está na posição ${i}`)
+ }
+
+
+/*for(let i=0; i<=num.length; i++){
     console.log(num[i])
-}
+}*/
