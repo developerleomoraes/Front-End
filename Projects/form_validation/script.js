69f7ac12a2943c985166069e631da963a3f1ca2d
leomoraes <developerleo.it@gmail.com>
2023-03-10 00:01:21 -0300

    JavaScript validation Project

--- Projects/form_validation/script.js
+++ Projects/form_validation/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form')
 const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
-const password = document.getElementById('password')
+const passwordInput = document.getElementById('password')
 const jobSelect = document.getElementById('job')
 const messageTextArea = document.getElementById('message')
 
@@ -22,9 +22,22 @@ form.addEventListener('submit', (event) => {
         return
     } 
 
-    // verificar se a está preenchido
-    if (){
+    // verificar se a senha está preenchido
+    if (!validatePassword(passwordInput.value, 8)){
+        alert('A senha precisa de no mínimo 8 dígitos')
+        return
+    }
 
+    // verificar se a situação foi selecionada
+    if (jobSelect.value === ''){
+        alert('Por favor, selecione a sua situação')
+        return
+    }
+
+    // verifica se a mensagem está preenchida
+    if (messageTextArea.valeu === ''){
+        alert('Por favor, preencha ou escreva uma mensagem')
+        return
     }
 
     // verifica se todos os commpos estão corretamente preenchidos
@@ -52,7 +65,6 @@ function validatePassword(password, minDigits) {
         // valid password
         return true 
     }
-
     // invalid password
     return false
 }
